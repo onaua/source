2027e27ef2e98c4bc9e00a18ae1db7a1f1a336f1
onaua <onaua@outlook.com>
2024-02-19 18:31:38 +0800

    2.19-6

--- temp/pre_gen_html.py
+++ temp/pre_gen_html.py
@@ -5,7 +5,7 @@ from rich import print,print_json
 from requests import Session
 from flask import Flask
 from pathlib import Path as __path
-
+url="https://onaua.github.io/source/"
 #写一个python脚本，使用pathlib库，实现遍历一个目录下的所有文件和文件夹，只遍历一层，并将其信息（文件绝对路径、文件名、文件大小、文件最后一次修改时间、文件创建时间）以json格式返回给前端。
 def get_file_info(path):
     import os
@@ -25,6 +25,7 @@ def get_file_info(path):
                 "type":"file",
                 "size":file_size,
                 "modified":modify_time,
+                "url":url+str(_content_.relative_to(rooooot)).replace("\\","/"),
                 
             })
         elif _content_.is_dir():
@@ -35,7 +36,9 @@ def get_file_info(path):
                 
                 "type":"dir",
                 "modified":modify_time,
+                "url":url+str(_content_.relative_to(rooooot)).replace("\\","/"),
             })     
+    print_json(data=result)
     return result
 
 import os
@@ -54,11 +57,11 @@ for ffff in rooooot.rglob("**/**"):
             json.dump(vvv,ff)
             ff.close()
             input(str(json_path))
-vvv=get_file_info(r"g:\take away\programs\project\toast")
+#vvv=get_file_info(r"g:\take away\programs\project\toast")
 #print_json(data=vvv)
-pp_=__path(r"g:\take away\programs\project\toast")
+#pp_=__path(r"g:\take away\programs\project\toast")
 
-json.dump(vvv,open(r"G:\take away\programs\project\temp\json\win.json","w",encoding="utf-8"))
+#json.dump(vvv,open(r"G:\take away\programs\project\temp\json\win.json","w",encoding="utf-8"))
 #print(list(pp_.glob("*")))
 #使用方法：
 #1、将该脚本放到一个目录下，如：g:\take away\programs\project\toast\pre_gen_html.py
