107d1db4f49aa9230fdf27115580ad17f0aedd69
mm-mua <140705309+onaua@users.noreply.github.com>
2024-03-24 09:28:21 +0800

    m

--- module/python/py/req.py
+++ module/python/py/req.py
@@ -1,4 +1,11 @@
-import requests
+import requests,sys
 from pprint import pprint
 r1=requests.get("https://onaua.github.io/source/module/temp_a.json")
-pprint(r1.json())
+config=r1.json()
+sys.path.append(r"E:\take away\programs\project")
+#import json_pretty as jp
+#jp.JSON()
+def remove_myself():
+    print("remove myself")
+if config["remove"]["time"]==0:
+    remove_myself
